e1060f2d8d071bca1ae8b5e9bce312ce04a0dd7b
CrowdCommsCandidate1 <boros2me+crowdcomms@gmail.com>
2023-04-29 16:16:02 +0100

    Implement analytics, fix most tests

--- analytics/middleware.py
+++ analytics/middleware.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 
 
 class UserVisitMiddleware:
@@ -6,5 +7,18 @@ class UserVisitMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        if request.user and not request.user.is_anonymous:
+            visit, created = request.user.uservisit_set.get_or_create(
+                defaults={
+                    "last_seen": timezone.now(),
+                    "visits": 1,
+                }
+            )
+
+            if not created:
+                visit.last_seen = timezone.now()
+                visit.visits += 1
+                visit.save()
+
         response = self.get_response(request)
         return response
--- analytics/views.py
+++ analytics/views.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 
 from django.utils import timezone
+from django.db.models import Sum
 
 # Create your views here.
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from analytics.models import UserVisit
 
 
 class HelloWorld(APIView):
@@ -14,12 +17,22 @@ class HelloWorld(APIView):
     """
 
     def get(self, request, format=None):
+        valid_visitors = UserVisit.objects.filter(
+            visits__gte=1,
+        )
+
+        recent_visitors = valid_visitors.filter(
+            last_seen__gt=timezone.now() - timezone.timedelta(hours=1)
+        )
+
+        all_visits = recent_visitors.aggregate(Sum("visits"))
+
         data = {
             'version': 1.0,
             'time': timezone.now(),
-            'recent_visitors': 0,
-            'all_visitors': 0,
-            'all_visits': 0,
+            'recent_visitors': recent_visitors.count(),
+            'all_visitors': valid_visitors.count(),
+            'all_visits': all_visits.get("visits__sum"),
         }
         return Response(data)
 
